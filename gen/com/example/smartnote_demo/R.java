4e08d946c78e0746d60f3ab5e6d55b9020a67a4f
Katarzyna Kuzawska <kasia.kuzawska@gmail.com>
2013-12-29 11:10:54 +0100

    task-1: gen/ updated

--- gen/com/example/smartnote_demo/R.java
+++ gen/com/example/smartnote_demo/R.java
@@ -8,7 +8,67 @@
 package com.example.smartnote_demo;
 
 public final class R {
+    public static final class array {
+        public static final int entries=0x7f040000;
+        public static final int names=0x7f040001;
+    }
     public static final class attr {
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int Items=0x7f010001;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int Names=0x7f010006;
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int SelectedItem=0x7f010002;
+        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int UseReflection=0x7f010000;
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int maxQuantity=0x7f010005;
+        /** <p>Must be a floating point value, such as "<code>1.2</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int maxTheta=0x7f010003;
+        /** <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int minQuantity=0x7f010004;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,25 +77,93 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int events=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_new_dir=0x7f020002;
+        public static final int notes=0x7f020003;
+        public static final int others=0x7f020004;
+        public static final int pictures=0x7f020005;
+        public static final int quick_notes=0x7f020006;
+        public static final int records=0x7f020007;
+        public static final int scanvas_bg=0x7f020008;
+        public static final int selector_eraser=0x7f020009;
+        public static final int selector_pen=0x7f02000a;
+        public static final int selector_redo=0x7f02000b;
+        public static final int selector_text=0x7f02000c;
+        public static final int selector_tool_bg=0x7f02000d;
+        public static final int selector_undo=0x7f02000e;
+        public static final int tool_ic_area_bg=0x7f02000f;
+        public static final int tool_ic_area_bg_press=0x7f020010;
+        public static final int tool_ic_area_center_02=0x7f020011;
+        public static final int tool_ic_area_select_02=0x7f020012;
+        public static final int tool_ic_eraser=0x7f020013;
+        public static final int tool_ic_eraser_dim=0x7f020014;
+        public static final int tool_ic_eraser_press=0x7f020015;
+        public static final int tool_ic_pen=0x7f020016;
+        public static final int tool_ic_pen_dim=0x7f020017;
+        public static final int tool_ic_pen_press=0x7f020018;
+        public static final int tool_ic_redo=0x7f020019;
+        public static final int tool_ic_redo_dim=0x7f02001a;
+        public static final int tool_ic_redo_press=0x7f02001b;
+        public static final int tool_ic_select=0x7f02001c;
+        public static final int tool_ic_select_dim=0x7f02001d;
+        public static final int tool_ic_select_press=0x7f02001e;
+        public static final int tool_ic_text=0x7f02001f;
+        public static final int tool_ic_text_dim=0x7f020020;
+        public static final int tool_ic_text_press=0x7f020021;
+        public static final int tool_ic_undo=0x7f020022;
+        public static final int tool_ic_undo_dim=0x7f020023;
+        public static final int tool_ic_undo_press=0x7f020024;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090013;
+        public static final int button1=0x7f090004;
+        public static final int button_ok=0x7f090002;
+        public static final int canvas_container=0x7f090007;
+        public static final int canvas_default_background=0x7f09000f;
+        public static final int carousel=0x7f090006;
+        public static final int editText1=0x7f090001;
+        public static final int eraseBtn=0x7f09000a;
+        public static final int gridView1=0x7f090003;
+        public static final int item_image=0x7f090010;
+        public static final int item_text=0x7f090011;
+        public static final int layout_container=0x7f09000e;
+        public static final int myselector=0x7f090012;
+        public static final int penBtn=0x7f090009;
+        public static final int redoBtn=0x7f09000d;
+        public static final int selected_item=0x7f090005;
+        public static final int textBtn=0x7f09000b;
+        public static final int textView1=0x7f090000;
+        public static final int tool_menu=0x7f090008;
+        public static final int undoBtn=0x7f09000c;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_canvas=0x7f030000;
+        public static final int activity_create_dir=0x7f030001;
+        public static final int activity_directories=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int clear_canvas=0x7f030004;
+        public static final int editor_basic_ui=0x7f030005;
+        public static final int item=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int canvas=0x7f080000;
+        public static final int create_dir=0x7f080001;
+        public static final int directories=0x7f080002;
+        public static final int main=0x7f080003;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int create_dir_label=0x7f060006;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_canvas=0x7f060003;
+        public static final int title_activity_create_dir=0x7f060005;
+        public static final int title_activity_directories=0x7f060004;
     }
     public static final class style {
         /** 
@@ -59,10 +187,153 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a Carousel.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #Carousel_Items com.example.smartnote_demo:Items}</code></td><td></td></tr>
+           <tr><td><code>{@link #Carousel_Names com.example.smartnote_demo:Names}</code></td><td></td></tr>
+           <tr><td><code>{@link #Carousel_SelectedItem com.example.smartnote_demo:SelectedItem}</code></td><td></td></tr>
+           <tr><td><code>{@link #Carousel_UseReflection com.example.smartnote_demo:UseReflection}</code></td><td></td></tr>
+           <tr><td><code>{@link #Carousel_android_animationDuration android:animationDuration}</code></td><td></td></tr>
+           <tr><td><code>{@link #Carousel_android_gravity android:gravity}</code></td><td></td></tr>
+           <tr><td><code>{@link #Carousel_maxQuantity com.example.smartnote_demo:maxQuantity}</code></td><td></td></tr>
+           <tr><td><code>{@link #Carousel_maxTheta com.example.smartnote_demo:maxTheta}</code></td><td></td></tr>
+           <tr><td><code>{@link #Carousel_minQuantity com.example.smartnote_demo:minQuantity}</code></td><td></td></tr>
+           </table>
+           @see #Carousel_Items
+           @see #Carousel_Names
+           @see #Carousel_SelectedItem
+           @see #Carousel_UseReflection
+           @see #Carousel_android_animationDuration
+           @see #Carousel_android_gravity
+           @see #Carousel_maxQuantity
+           @see #Carousel_maxTheta
+           @see #Carousel_minQuantity
+         */
+        public static final int[] Carousel = {
+            0x010100af, 0x01010112, 0x7f010000, 0x7f010001,
+            0x7f010002, 0x7f010003, 0x7f010004, 0x7f010005,
+            0x7f010006
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.example.smartnote_demo.R.attr#Items}
+          attribute's value can be found in the {@link #Carousel} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.smartnote_demo:Items
+        */
+        public static final int Carousel_Items = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.example.smartnote_demo.R.attr#Names}
+          attribute's value can be found in the {@link #Carousel} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.smartnote_demo:Names
+        */
+        public static final int Carousel_Names = 8;
+        /**
+          <p>This symbol is the offset where the {@link com.example.smartnote_demo.R.attr#SelectedItem}
+          attribute's value can be found in the {@link #Carousel} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.smartnote_demo:SelectedItem
+        */
+        public static final int Carousel_SelectedItem = 4;
+        /**
+          <p>This symbol is the offset where the {@link com.example.smartnote_demo.R.attr#UseReflection}
+          attribute's value can be found in the {@link #Carousel} array.
+
+
+          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.smartnote_demo:UseReflection
+        */
+        public static final int Carousel_UseReflection = 2;
+        /**
+          <p>This symbol is the offset where the {@link android.R.attr#animationDuration}
+          attribute's value can be found in the {@link #Carousel} array.
+          @attr name android:animationDuration
+        */
+        public static final int Carousel_android_animationDuration = 1;
+        /**
+          <p>This symbol is the offset where the {@link android.R.attr#gravity}
+          attribute's value can be found in the {@link #Carousel} array.
+          @attr name android:gravity
+        */
+        public static final int Carousel_android_gravity = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.example.smartnote_demo.R.attr#maxQuantity}
+          attribute's value can be found in the {@link #Carousel} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.smartnote_demo:maxQuantity
+        */
+        public static final int Carousel_maxQuantity = 7;
+        /**
+          <p>This symbol is the offset where the {@link com.example.smartnote_demo.R.attr#maxTheta}
+          attribute's value can be found in the {@link #Carousel} array.
+
+
+          <p>Must be a floating point value, such as "<code>1.2</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.smartnote_demo:maxTheta
+        */
+        public static final int Carousel_maxTheta = 5;
+        /**
+          <p>This symbol is the offset where the {@link com.example.smartnote_demo.R.attr#minQuantity}
+          attribute's value can be found in the {@link #Carousel} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.smartnote_demo:minQuantity
+        */
+        public static final int Carousel_minQuantity = 6;
+    };
 }
